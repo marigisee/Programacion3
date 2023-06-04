482cb8e90e2fde44a8b20ed999495e3f56cffd18
Marina Cuello <marigisee@gmail.com>
2023-06-04 19:29:33 -0300

    upload of test and methods used in exercise 5

---
+++ src/prog3/grafos/test/testRecorridos.java
@@ -0,0 +1,58 @@
+package prog3.grafos.test;
+import prog3.grafos.*;
+import prog3.listagenerica.*;
+import prog3.utiles.Recorridos;
+
+public class testRecorridos {
+    public static void main(String[] args) {
+
+        Vertice<String> v1 = new VerticeImplListAdy<String>("Buenos Aires");
+        Vertice<String> v2 = new VerticeImplListAdy<String>("Santiago");
+        Vertice<String> v3 = new VerticeImplListAdy<String>("Lima");
+        Vertice<String> v4 = new VerticeImplListAdy<String>("Montevideo");
+        Vertice<String> v5 = new VerticeImplListAdy<String>("Asuncion");
+        Vertice<String> v6 = new VerticeImplListAdy<String>("Caracas");
+        Vertice<String> v7 = new VerticeImplListAdy<String>("La Habana");
+
+        Grafo<String> ciudades = new GrafoImplListAdy<String>();
+
+        ciudades.agregarVertice(v1);
+        ciudades.agregarVertice(v2);
+        ciudades.agregarVertice(v3);
+        ciudades.agregarVertice(v4);
+        ciudades.agregarVertice(v5);
+        ciudades.agregarVertice(v6);
+        ciudades.agregarVertice(v7);
+
+        ciudades.conectar(v1, v2);
+        ciudades.conectar(v1, v3);
+        ciudades.conectar(v1, v4);
+        ciudades.conectar(v1, v5);
+        ciudades.conectar(v2, v5);
+        ciudades.conectar(v2, v7);
+        ciudades.conectar(v3, v5);
+        ciudades.conectar(v4, v5);
+        ciudades.conectar(v5, v6);
+        ciudades.conectar(v6, v7);
+
+
+
+        Recorridos<String> r = new Recorridos<String>();
+        ListaGenerica <Vertice<String>> lisDFS = r.dfs(ciudades);
+        System.out.println("---Se imprime el GRAFO con DFS---");
+        lisDFS.comenzar();
+        while(!lisDFS.fin()) {
+            System.out.println(lisDFS.proximo().dato());
+        }
+
+        ListaGenerica <Vertice<String>> lisBFS = r.bfs(ciudades);
+        System.out.println("---Se imprime el GRAFO con BFS---");
+        lisBFS.comenzar();
+        while(!lisBFS.fin()) {
+            System.out.println(lisBFS.proximo().dato());
+        }
+
+
+    }
+}
+
---
+++ src/prog3/utiles/Recorridos.java
@@ -0,0 +1,80 @@
+package prog3.utiles;
+import prog3.grafos.*;
+import prog3.listagenerica.*;
+import prog3.util.*;
+
+public class Recorridos<T> {
+    public ListaGenerica<Vertice<T>> dfs(Grafo<T> grafo){
+        //Arreglo marca el cual nos va a decir cual fue utilizado como vértice inicial y cual no
+        boolean[] marca = new boolean[grafo.listaDeVertices().tamanio()];
+        ListaGenerica<Vertice<T>> recorrido = new ListaGenericaEnlazada<>();
+        for (int i = 0; i<marca.length; i++){
+            if (!marca[i]) {
+                this.dfs(i,grafo,recorrido,marca);
+            }
+        }
+     return recorrido;}
+
+    /**
+     * @param p: posición del vértice con el que iniciamos a recorrer
+     * @param recorrido: ListaGenérica que contiene todos los vértices que están en el recorrido
+     */
+
+    private void dfs (int p, Grafo<T> grafo, ListaGenerica<Vertice<T>> recorrido, boolean[] marca) {
+            //Cambio el valor a true del vector marca
+            marca[p] = true;
+            //Creo variable auxiliar vértice
+            Vertice<T> v = grafo.listaDeVertices().elemento(p);
+            //Agrego el vértice actual al recorrido
+            recorrido.agregarFinal(v);
+            //Creo una variable auxiliar listaAdyacentes
+            ListaGenerica<Arista<T>> ady = grafo.listaDeAdyacentes(v);
+            ady.comenzar();
+            while (!ady.fin()) {
+                    int posicion_vertice = ady.proximo().verticeDestino().posicion();
+                    if (!marca[posicion_vertice]){
+                        this.dfs(posicion_vertice,grafo,recorrido,marca);
+                    }
+                }
+            }
+
+
+    public  ListaGenerica<Vertice<T>> bfs (Grafo<T> grafo){
+        //Lista donde voy a almacenar el recorrido del grafo
+        ListaGenerica<Vertice<T>> recorrido = new ListaGenericaEnlazada<>();
+        //Vector de marcas
+        boolean[] marca = new boolean[grafo.listaDeVertices().tamanio()];
+        for (int i=0;i<marca.length;i++){
+             if (!marca[i]) {
+                 this.bfs(i,grafo,recorrido,marca);
+             }
+        }
+        return recorrido;
+    }
+    private void bfs (int i,Grafo<T> grafo,  ListaGenerica<Vertice<T>> recorrido,boolean[] marca) {
+        //Lista donde voy a almacenar los adyacentes del vértice al que estoy procesando
+        ListaGenerica<Arista<T>> ady = null;
+        //Cola genérica
+        ColaGenerica<Vertice<T>> cola = new ColaGenerica<>();
+        cola.encolar(grafo.listaDeVertices().elemento(i));
+        marca[i] = true;
+        while (!cola.esVacia()){
+            Vertice<T> v = cola.desencolar();
+            recorrido.agregarFinal(v);
+            ady = grafo.listaDeAdyacentes(v);
+            ady.comenzar();
+            while (!(ady.fin())){
+                Arista<T> arista = ady.proximo();
+                int p = arista.verticeDestino().posicion();
+                if (!marca[p]) {
+                     Vertice<T> w = arista.verticeDestino();
+                     marca[p] = true;
+                     cola.encolar(w);
+                }
+            }
+        }
+    }
+}
+
+
+
